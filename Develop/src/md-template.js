d0c8d00b30a9e8b1efee21720f766dd5f39ae159
rfabreu <raabreugomes@gmail.com>
2022-07-07 02:28:30 -0400

    Create initial template file

---
+++ Develop/src/md-template.js
@@ -0,0 +1,28 @@
+// create project data template
+module.exports = projectData => {
+const generateProjects = projectData;
+    return `
+    # ${title}
+    ${licenseBadge}
+    - [${title}](#${title})
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    ## Description
+    ${description}
+    ## Installation
+    ${install}
+    ## Usage
+    ${use}
+    ## Contributing
+    ${contribution}
+    ## Tests
+    ${test}
+    ## Questions
+    GitHub: [github.com/${username}](https://github.com/${username})
+    Comments, critiques, questions? Contact me at: [${email}](mailto:${email})
+    `;
+};
